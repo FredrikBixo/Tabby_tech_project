ddb4d827dba7cfe23aee707b6dc919982f801403
LeFilipTran <le5116tr-s@student.lu.se>
2021-05-11 18:00:54 +0200

    add sceneviewer to test catch page

--- app/src/main/java/com/google/ar/core/examples/java/helloar/MenuActivity.java
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/MenuActivity.java
@@ -1,11 +1,12 @@
 package com.google.ar.core.examples.java.helloar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -24,6 +25,14 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+        button = (Button) findViewById(R.id.SceneViewer);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openScene();
+            }
+        });
+
         /*button = (Button) findViewById(R.id.achButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +47,18 @@ public class MenuActivity extends AppCompatActivity {
     public void openPlay() {
         Intent intent = new Intent(this, HelloArActivity.class);
         startActivity(intent);
+    }
+
+    public void openScene() {
+        Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+        Uri intentUri =
+                Uri.parse("https://arvr.google.com/scene-viewer/1.0").buildUpon()
+                        .appendQueryParameter("file", "https://raw.githubusercontent.com/FredrikBixo/Tabby_tech_project/main/app/src/main/assets/models/butterfly.gltf")
+                        .appendQueryParameter("mode", "ar_only")
+                        .build();
+        sceneViewerIntent.setData(intentUri);
+        sceneViewerIntent.setPackage("com.google.ar.core");
+        startActivity(sceneViewerIntent);
     }
     /*public void openAch() {
         Intent intent = new Intent(this, ÄNDRA_DEN_HÄR_KLASSEN.class);
