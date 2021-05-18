552b29998058d40dd73d26cdb372cab29fb9e976
Ika J. Injac <ika.injac@gmail.com>
2021-05-18 23:11:16 +0200

    ButterflyInfo.java tillagd

--- app/src/main/java/com/google/ar/core/examples/java/helloar/CollectionActivity.java
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/CollectionActivity.java
@@ -2,7 +2,9 @@ package com.google.ar.core.examples.java.helloar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CollectionActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class CollectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection);
+    }
+
+    public void onClick1(View view) {
+        Intent intent = new Intent(this, ButterflyInfo.class);
+        intent.putExtra("butterfly_id",1);
+        startActivity(intent);
+    }
+
+    public void onClick2(View view) {
+        Intent intent = new Intent(this, ButterflyInfo.class);
+        intent.putExtra("butterfly_id",2);
+        startActivity(intent);
+    }
+
+    public void onClick3(View view) {
+        Intent intent = new Intent(this, ButterflyInfo.class);
+        intent.putExtra("butterfly_id",3);
+        startActivity(intent);
     }
 }
