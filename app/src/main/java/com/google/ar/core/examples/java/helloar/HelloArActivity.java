5af989322c69eeb8303172aeb221584f0fb4f4e8
LeFilipTran <le5116tr-s@student.lu.se>
2021-05-19 01:43:39 +0200

    add camera preview

--- app/src/main/java/com/google/ar/core/examples/java/helloar/HelloArActivity.java
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/HelloArActivity.java
@@ -16,44 +16,10 @@
 
 package com.google.ar.core.examples.java.helloar;
 
-import android.opengl.GLSurfaceView;
-import android.opengl.Matrix;
+import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.ar.core.ArCoreApk;
-import com.google.ar.core.Camera;
-import com.google.ar.core.Config;
-import com.google.ar.core.Frame;
-import com.google.ar.core.LightEstimate;
-import com.google.ar.core.Plane;
-import com.google.ar.core.Session;
-import com.google.ar.core.TrackingState;
-import com.google.ar.core.examples.java.common.helpers.CameraPermissionHelper;
-import com.google.ar.core.examples.java.common.helpers.DepthSettings;
-import com.google.ar.core.examples.java.common.helpers.DisplayRotationHelper;
-import com.google.ar.core.examples.java.common.helpers.FullScreenHelper;
-import com.google.ar.core.examples.java.common.helpers.SnackbarHelper;
-import com.google.ar.core.examples.java.common.helpers.TapHelper;
-import com.google.ar.core.examples.java.common.helpers.TrackingStateHelper;
-import com.google.ar.core.examples.java.common.samplerender.Framebuffer;
-import com.google.ar.core.examples.java.common.samplerender.Mesh;
-import com.google.ar.core.examples.java.common.samplerender.SampleRender;
-import com.google.ar.core.examples.java.common.samplerender.Shader;
-import com.google.ar.core.examples.java.common.samplerender.Texture;
-import com.google.ar.core.examples.java.common.samplerender.arcore.BackgroundRenderer;
-import com.google.ar.core.examples.java.common.samplerender.arcore.SpecularCubemapFilter;
-import com.google.ar.core.exceptions.CameraNotAvailableException;
-import com.google.ar.core.exceptions.UnavailableApkTooOldException;
-import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
-import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
-import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
-import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
-
-import java.io.IOException;
+import android.widget.FrameLayout;
 
 
 /**
@@ -61,356 +27,33 @@ import java.io.IOException;
  * ARCore API. The application will display any detected planes and will allow the user to tap on a
  * plane to place a 3D model.
  */
-public class HelloArActivity extends AppCompatActivity implements SampleRender.Renderer {
+public class HelloArActivity extends Activity {
 
-  private static final String TAG = HelloArActivity.class.getSimpleName();
-
-  // See the definition of updateSphericalHarmonicsCoefficients for an explanation of these
-  // constants.
-  private static final float[] sphericalHarmonicFactors = {
-          0.282095f,
-          -0.325735f,
-          0.325735f,
-          -0.325735f,
-          0.273137f,
-          -0.273137f,
-          0.078848f,
-          -0.273137f,
-          0.136569f,
-  };
-
-  private static final float Z_NEAR = 0.1f;
-  private static final float Z_FAR = 100f;
-
-  // Rendering. The Renderers are created here, and initialized when the GL surface is created.
-  private GLSurfaceView surfaceView;
-
-  private boolean installRequested;
-
-  private Session session;
-  private final SnackbarHelper messageSnackbarHelper = new SnackbarHelper();
-  private DisplayRotationHelper displayRotationHelper;
-  private final TrackingStateHelper trackingStateHelper = new TrackingStateHelper(this);
-  private TapHelper tapHelper;
-  private SampleRender render;
-
-  private BackgroundRenderer backgroundRenderer;
-  private Framebuffer virtualSceneFramebuffer;
-  private boolean hasSetTextureNames = false;
-
-  private final DepthSettings depthSettings = new DepthSettings();
-
-
-  // Virtual object (ARCore pawn)
-  private Mesh virtualObjectMesh;
-  private Shader virtualObjectShader;
-
-  // Environmental HDR
-  private Texture dfgTexture;
-  private SpecularCubemapFilter cubemapFilter;
-
-  // Temporary matrix allocated here to reduce number of allocations for each frame.
-  private final float[] sphericalHarmonicsCoefficients = new float[9 * 3];
-  private final float[] viewInverseMatrix = new float[16];
-  private final float[] worldLightDirection = {0.0f, 0.0f, 0.0f, 0.0f};
-  private final float[] viewLightDirection = new float[4]; // view x world light direction
-
-  
+  private Camera mCamera;
+  private CameraPreview mPreview;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_ar);
-    surfaceView = findViewById(R.id.surfaceview);
-    displayRotationHelper = new DisplayRotationHelper(/*context=*/ this);
 
-    // Set up touch listener.
-    tapHelper = new TapHelper(/*context=*/ this);
-    surfaceView.setOnTouchListener(tapHelper);
+    // Create an instance of Camera
+    mCamera = getCameraInstance();
 
-    // Set up renderer.
-    render = new SampleRender(surfaceView, this, getAssets());
-
-    installRequested = false;
-
+    // Create our Preview view and set it as the content of our activity.
+    mPreview = new CameraPreview(this, mCamera);
+    FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+    preview.addView(mPreview);
   }
 
-  @Override
-  protected void onDestroy() {
-    if (session != null) {
-      // Explicitly close ARCore Session to release native resources.
-      // Review the API reference for important considerations before calling close() in apps with
-      // more complicated lifecycle requirements:
-      // https://developers.google.com/ar/reference/java/arcore/reference/com/google/ar/core/Session#close()
-      session.close();
-      session = null;
-    }
-
-    super.onDestroy();
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-
-    if (session == null) {
-      Exception exception = null;
-      String message = null;
-      try {
-        switch (ArCoreApk.getInstance().requestInstall(this, !installRequested)) {
-          case INSTALL_REQUESTED:
-            installRequested = true;
-            return;
-          case INSTALLED:
-            break;
-        }
-
-        // ARCore requires camera permissions to operate. If we did not yet obtain runtime
-        // permission on Android M and above, now is a good time to ask the user for it.
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-          CameraPermissionHelper.requestCameraPermission(this);
-          return;
-        }
-
-        // Create the session.
-        session = new Session(/* context= */ this);
-      } catch (UnavailableArcoreNotInstalledException
-              | UnavailableUserDeclinedInstallationException e) {
-        message = "Please install ARCore";
-        exception = e;
-      } catch (UnavailableApkTooOldException e) {
-        message = "Please update ARCore";
-        exception = e;
-      } catch (UnavailableSdkTooOldException e) {
-        message = "Please update this app";
-        exception = e;
-      } catch (UnavailableDeviceNotCompatibleException e) {
-        message = "This device does not support AR";
-        exception = e;
-      } catch (Exception e) {
-        message = "Failed to create AR session";
-        exception = e;
-      }
-
-      if (message != null) {
-        messageSnackbarHelper.showError(this, message);
-        Log.e(TAG, "Exception creating session", exception);
-        return;
-      }
-    }
-
-    // Note that order matters - see the note in onPause(), the reverse applies here.
+  public static Camera getCameraInstance(){
+    Camera c = null;
     try {
-      configureSession();
-      // To record a live camera session for later playback, call
-      // `session.startRecording(recorderConfig)` at anytime. To playback a previously recorded AR
-      // session instead of using the live camera feed, call
-      // `session.setPlaybackDataset(playbackDatasetPath)` before calling `session.resume()`. To
-      // learn more about recording and playback, see:
-      // https://developers.google.com/ar/develop/java/recording-and-playback
-      session.resume();
-    } catch (CameraNotAvailableException e) {
-      messageSnackbarHelper.showError(this, "Camera not available. Try restarting the app.");
-      session = null;
-      return;
+      c = Camera.open(); // attempt to get a Camera instance
     }
-
-    surfaceView.onResume();
-    displayRotationHelper.onResume();
+    catch (Exception e){
+      // Camera is not available (in use or does not exist)
+    }
+    return c; // returns null if camera is unavailable
   }
-
-  @Override
-  public void onPause() {
-    super.onPause();
-    if (session != null) {
-      // Note that the order matters - GLSurfaceView is paused first so that it does not try
-      // to query the session. If Session is paused before GLSurfaceView, GLSurfaceView may
-      // still call session.update() and get a SessionPausedException.
-      displayRotationHelper.onPause();
-      surfaceView.onPause();
-      session.pause();
-    }
-  }
-
-  @Override
-  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
-    super.onRequestPermissionsResult(requestCode, permissions, results);
-    if (!CameraPermissionHelper.hasCameraPermission(this)) {
-      // Use toast instead of snackbar here since the activity will exit.
-      Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
-              .show();
-      if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
-        // Permission denied with checking "Do not ask again".
-        CameraPermissionHelper.launchPermissionSettings(this);
-      }
-      finish();
-    }
-  }
-
-  @Override
-  public void onWindowFocusChanged(boolean hasFocus) {
-    super.onWindowFocusChanged(hasFocus);
-    FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus);
-  }
-
-  @Override
-  public void onSurfaceCreated(SampleRender render) {
-    // Prepare the rendering objects. This involves reading shaders and 3D model files, so may throw
-    // an IOException.
-
-    backgroundRenderer = new BackgroundRenderer(render);
-    virtualSceneFramebuffer = new Framebuffer(render, /*width=*/ 1, /*height=*/ 1);
-
-  }
-
-  @Override
-  public void onSurfaceChanged(SampleRender render, int width, int height) {
-    displayRotationHelper.onSurfaceChanged(width, height);
-    virtualSceneFramebuffer.resize(width, height);
-  }
-
-  @Override
-  public void onDrawFrame(SampleRender render) {
-    if (session == null) {
-      return;
-    }
-
-    // Texture names should only be set once on a GL thread unless they change. This is done during
-    // onDrawFrame rather than onSurfaceCreated since the session is not guaranteed to have been
-    // initialized during the execution of onSurfaceCreated.
-    if (!hasSetTextureNames) {
-      session.setCameraTextureNames(
-              new int[] {backgroundRenderer.getCameraColorTexture().getTextureId()});
-      hasSetTextureNames = true;
-    }
-
-    // -- Update per-frame state
-
-    // Notify ARCore session that the view size changed so that the perspective matrix and
-    // the video background can be properly adjusted.
-    displayRotationHelper.updateSessionIfNeeded(session);
-
-    // Obtain the current frame from ARSession. When the configuration is set to
-    // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
-    // camera framerate.
-    Frame frame;
-    try {
-      frame = session.update();
-    } catch (CameraNotAvailableException e) {
-      Log.e(TAG, "Camera not available during onDrawFrame", e);
-      messageSnackbarHelper.showError(this, "Camera not available. Try restarting the app.");
-      return;
-    }
-    Camera camera = frame.getCamera();
-
-    // Update BackgroundRenderer state to match the depth settings.
-    try {
-      backgroundRenderer.setUseDepthVisualization(
-              render, depthSettings.depthColorVisualizationEnabled());
-      backgroundRenderer.setUseOcclusion(render, depthSettings.useDepthForOcclusion());
-    } catch (IOException e) {
-      Log.e(TAG, "Failed to read a required asset file", e);
-      messageSnackbarHelper.showError(this, "Failed to read a required asset file: " + e);
-      return;
-    }
-    // BackgroundRenderer.updateDisplayGeometry must be called every frame to update the coordinates
-    // used to draw the background camera image.
-    backgroundRenderer.updateDisplayGeometry(frame);
-
-    // Keep the screen unlocked while tracking, but allow it to lock when tracking stops.
-    trackingStateHelper.updateKeepScreenOnFlag(camera.getTrackingState());
-
-    // -- Draw background
-
-    if (frame.getTimestamp() != 0) {
-      // Suppress rendering if the camera did not produce the first frame yet. This is to avoid
-      // drawing possible leftover data from previous sessions if the texture is reused.
-      backgroundRenderer.drawBackground(render);
-    }
-
-    // Compose the virtual scene with the background.
-    backgroundRenderer.drawVirtualScene(render, virtualSceneFramebuffer, Z_NEAR, Z_FAR);
-  }
-
-
-  /** Checks if we detected at least one plane. */
-  private boolean hasTrackingPlane() {
-    for (Plane plane : session.getAllTrackables(Plane.class)) {
-      if (plane.getTrackingState() == TrackingState.TRACKING) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  /** Update state based on the current frame's light estimation. */
-  private void updateLightEstimation(LightEstimate lightEstimate, float[] viewMatrix) {
-    if (lightEstimate.getState() != LightEstimate.State.VALID) {
-      virtualObjectShader.setBool("u_LightEstimateIsValid", false);
-      return;
-    }
-    virtualObjectShader.setBool("u_LightEstimateIsValid", true);
-
-    Matrix.invertM(viewInverseMatrix, 0, viewMatrix, 0);
-    virtualObjectShader.setMat4("u_ViewInverse", viewInverseMatrix);
-
-    updateMainLight(
-            lightEstimate.getEnvironmentalHdrMainLightDirection(),
-            lightEstimate.getEnvironmentalHdrMainLightIntensity(),
-            viewMatrix);
-    updateSphericalHarmonicsCoefficients(
-            lightEstimate.getEnvironmentalHdrAmbientSphericalHarmonics());
-    cubemapFilter.update(lightEstimate.acquireEnvironmentalHdrCubeMap());
-  }
-
-  private void updateMainLight(float[] direction, float[] intensity, float[] viewMatrix) {
-    // We need the direction in a vec4 with 0.0 as the final component to transform it to view space
-    worldLightDirection[0] = direction[0];
-    worldLightDirection[1] = direction[1];
-    worldLightDirection[2] = direction[2];
-    Matrix.multiplyMV(viewLightDirection, 0, viewMatrix, 0, worldLightDirection, 0);
-    virtualObjectShader.setVec4("u_ViewLightDirection", viewLightDirection);
-    virtualObjectShader.setVec3("u_LightIntensity", intensity);
-  }
-
-  private void updateSphericalHarmonicsCoefficients(float[] coefficients) {
-    // Pre-multiply the spherical harmonics coefficients before passing them to the shader. The
-    // constants in sphericalHarmonicFactors were derived from three terms:
-    //
-    // 1. The normalized spherical harmonics basis functions (y_lm)
-    //
-    // 2. The lambertian diffuse BRDF factor (1/pi)
-    //
-    // 3. A <cos> convolution. This is done to so that the resulting function outputs the irradiance
-    // of all incoming light over a hemisphere for a given surface normal, which is what the shader
-    // (environmental_hdr.frag) expects.
-    //
-    // You can read more details about the math here:
-    // https://google.github.io/filament/Filament.html#annex/sphericalharmonics
-
-    if (coefficients.length != 9 * 3) {
-      throw new IllegalArgumentException(
-              "The given coefficients array must be of length 27 (3 components per 9 coefficients");
-    }
-
-    // Apply each factor to every component of each coefficient
-    for (int i = 0; i < 9 * 3; ++i) {
-      sphericalHarmonicsCoefficients[i] = coefficients[i] * sphericalHarmonicFactors[i / 3];
-    }
-    virtualObjectShader.setVec3Array(
-            "u_SphericalHarmonicsCoefficients", sphericalHarmonicsCoefficients);
-  }
-
-  /** Configures the session with feature settings. */
-  private void configureSession() {
-    Config config = session.getConfig();
-    config.setLightEstimationMode(Config.LightEstimationMode.ENVIRONMENTAL_HDR);
-    if (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
-      config.setDepthMode(Config.DepthMode.AUTOMATIC);
-    } else {
-      config.setDepthMode(Config.DepthMode.DISABLED);
-    }
-    session.configure(config);
-  }
-
 }
